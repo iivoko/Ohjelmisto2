85cb375e6f5654b171fd32e8aff4075bc826f6b0
Iivo <iivo.koski@gmail.com>
2023-10-25 10:48:35 +0300

    .

---
+++ mod09/9.2.py
@@ -0,0 +1,23 @@
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, tämänhetkinen_nopeus, kuljettu_matka):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.tämänhetkinen_nopeus = tämänhetkinen_nopeus
+        self.kuljettu_matka = kuljettu_matka
+
+    def kiihdytä(self, nopeus):
+        self.tämänhetkinen_nopeus = self.tämänhetkinen_nopeus + nopeus
+        return
+
+auto = Auto("ABC-123", "142 km/h", 0, 0)
+
+
+auto.kiihdytä(30)
+auto.kiihdytä(70)
+auto.kiihdytä(50)
+
+print(f"auton nopeus on {auto.tämänhetkinen_nopeus}km/h")
+
+auto.kiihdytä(-200)
+
+print(f"auton nopeus on {auto.tämänhetkinen_nopeus}km/h")
