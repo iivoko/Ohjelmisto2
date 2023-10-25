22ede0266165524219b228f50286d4e25bef7500
Iivo <iivo.koski@gmail.com>
2023-10-25 10:06:14 +0300

    .

--- mod09/9.1.py
+++ mod09/9.1.py
@@ -1,1 +1,10 @@
-a
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, tämänhetkinen_nopeus, kuljettu_matka):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.tämänhetkinen_nopeus = tämänhetkinen_nopeus
+        self.kuljettu_matka = kuljettu_matka
+
+auto = Auto("ABC-123", "142 km/h", 0, 0)
+
+print(f"auton rekisteritunnus on {auto.rekisteritunnus},huippunopeus {auto.huippunopeus}, tämänhetkinen nopeus {auto.tämänhetkinen_nopeus}, kuljettu matka {auto.kuljettu_matka} ")
