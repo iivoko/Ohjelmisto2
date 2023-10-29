6636f515115a9ba23b1679c2c09acdef79f7b6c5
iivoko <iivo.koski@gmail.com>
2023-10-29 21:47:55 +0200

    jep

--- mod10/10.3.py
+++ mod10/10.3.py
@@ -23,7 +23,6 @@ class Hissi:
             self.sijainti = self.ylin
         elif self.sijainti < self.alin:
             self.sijainti = self.alin
-        print("hissi on nyt kohteessa")
         return
 
 
@@ -43,9 +42,16 @@ class Talo:
         return
 
     def palohälytys(self):
+        print("Palohälytys soi. Siirretään hissit alas")
         for i, hissi in enumerate(self.hissit):
             self.aja_hissia(i + 1, self.alin)
+        print("Kaikki hissit on nyt siirretty alas")
         return
 
 talo = Talo(1, 10, 5)
+talo.aja_hissia(1,3)
+talo.aja_hissia(2,4)
+talo.aja_hissia(3,2)
+talo.aja_hissia(4,9)
+talo.aja_hissia(5,7)
 talo.palohälytys()
