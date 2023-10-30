babe149eda90ac1ee8c7fcb738a862c1b74e0664
Iivo <iivo.koski@gmail.com>
2023-10-30 10:37:35 +0200

    .

---
+++ mod11/11.1.py
@@ -0,0 +1,27 @@
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+
+class Kirja(Julkaisu):
+    def __init__(self, nimi, kirjoittaja, sivumäärä):
+        self.kirjoittaja = kirjoittaja
+        self.sivumäärä = sivumäärä
+        super().__init__(nimi)
+    def tulosta_tiedot(self):
+        print(f"{self.nimi}, {self.kirjoittaja} {self.sivumäärä}")
+
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, päätoimittaja):
+        self.päätoimittaja = päätoimittaja
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        print(f"{self.nimi} {self.päätoimittaja}")
+
+lehti = Lehti("Aku Ankka", "Aki Hyyppä")
+kirja = Kirja("Hytti n:o 6", "Rosa Liksom", 200)
+
+lehti.tulosta_tiedot()
+kirja.tulosta_tiedot()
